bffced3894d023a3c4e31aef7d3803cb57546100
Prajakta-Dhale <141712592+Prajakta-Dhale@users.noreply.github.com>
2025-11-28 11:24:16 +0000

    User2

---
+++ userproject/src/components/User.jsx
@@ -0,0 +1,6 @@
+const users=[
+    {id:1,name:"Alice",email:"a@gmail.com",mobile:789456},
+    {id:2,name:"Booo",email:"b@gmail.com",mobile:123456},
+    {id:2,name:"praj",email:"p@gmail.com",mobile:783698}
+]
+export default users;
